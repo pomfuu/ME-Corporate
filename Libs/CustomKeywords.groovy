e93f93b0da5d8ab602f7133292048de533bdeccd
pomff <alincahyono@gmail.com>
2025-01-21 20:41:14 +0700

    .

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -3,25 +3,3 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
-import com.kms.katalon.core.testobject.TestObject
-
-import java.lang.String
-
-
-
-def static "popup.popup.uploadFile"(
-    	TestObject to	
-     , 	String filePath	) {
-    (new popup.popup()).uploadFile(
-        	to
-         , 	filePath)
-}
-
-
-def static "popup.popup.writeString"(
-    	String text	
-     , 	boolean includeEnter	) {
-    (new popup.popup()).writeString(
-        	text
-         , 	includeEnter)
-}
